efcedd5f3fcc2656edd47a2c3a5c160708cb0d64
Lazar Karic <16634314+lkaric@users.noreply.github.com>
2025-01-17 19:14:02 +0100

    fix: :wheelchair: added logo alt & fixed header flickering

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router';
 const Layout: React.FC = () => {
   return (
     <>
-      <header className="max-w-screen-xl m-2 xl:mx-auto xl:my-4">
-        <img src="/logo.svg" />
+      <header className="max-w-screen-xl m-2 xl:mx-auto xl:my-4 h-[40px]">
+        <img src="/logo.svg" alt="Positions" />
       </header>
       <main className="max-w-screen-xl m-2 xl:mx-auto">
         <Outlet />
