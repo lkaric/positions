fdcd372ff4c08868bd2f055f743916618b6d5d8e
Lazar Karic <16634314+lkaric@users.noreply.github.com>
2025-01-17 18:15:23 +0100

    feat: :technologist: added prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/en/configuration
+ * @type {import("prettier").Config}
+ */
+const prettierConfig = {
+  singleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  tabWidth: 2,
+};
+
+module.exports = prettierConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import { App } from "./App.tsx";
+import { App } from './App.tsx';
 
-const rootElement: HTMLElement = document.getElementById("root")!;
+const rootElement: HTMLElement = document.getElementById('root')!;
 
 createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
